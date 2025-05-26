71862d9278c85daa68c3d0dadc0c93bcb43b1125
xen-42 <nicholasconnors@hotmail.ca>
2025-05-25 23:27:22 -0400

    Set up rendering for the maple boat entity

--- src/client/java/xen42/canadamod/CanadaModClient.java
+++ src/client/java/xen42/canadamod/CanadaModClient.java
@@ -1,10 +1,37 @@
 package xen42.canadamod;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.model.TexturedModelData;
+import net.minecraft.client.render.entity.BoatEntityRenderer;
+import net.minecraft.client.render.entity.model.BoatEntityModel;
+import net.minecraft.client.render.entity.model.EntityModelLayer;
+import net.minecraft.util.Identifier;
 
 public class CanadaModClient implements ClientModInitializer {
+	public static final EntityModelLayer MAPLE_BOAT = new EntityModelLayer(Identifier.of(CanadaMod.MOD_ID, "boat/maple"), "main");
+	public static final EntityModelLayer MAPLE_CHEST_BOAT = new EntityModelLayer(Identifier.of(CanadaMod.MOD_ID, "chest_boat/maple"), "main");
+
+	public class MapleBoat implements EntityModelLayerRegistry.TexturedModelDataProvider {
+		@Override
+		public TexturedModelData createModelData() {
+			return BoatEntityModel.getTexturedModelData();
+		}
+	}
+
+	public class MapleChestBoat implements EntityModelLayerRegistry.TexturedModelDataProvider {
+		@Override
+		public TexturedModelData createModelData() {
+			return BoatEntityModel.getChestTexturedModelData();
+		}
+	}
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		EntityRendererRegistry.register(MapleBoatEntity.MAPLE_BOAT, context -> new BoatEntityRenderer(context, MAPLE_BOAT));
+		EntityRendererRegistry.register(MapleBoatEntity.MAPLE_CHEST_BOAT, context -> new BoatEntityRenderer(context, MAPLE_CHEST_BOAT));
+		EntityModelLayerRegistry.registerModelLayer(MAPLE_BOAT, new MapleBoat());
+		EntityModelLayerRegistry.registerModelLayer(MAPLE_CHEST_BOAT, new MapleChestBoat());
 	}
 }
