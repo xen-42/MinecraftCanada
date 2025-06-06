6fe103273760d881304b7fed9a206742041bd253
xen-42 <nicholasconnors@hotmail.ca>
2025-06-06 14:18:26 -0400

    it was not hardcoded that was a lie

--- src/client/java/xen42/canadamod/entity/MooseEntityRenderState.java
+++ src/client/java/xen42/canadamod/entity/MooseEntityRenderState.java
@@ -4,7 +4,6 @@ import net.minecraft.client.render.entity.state.LivingEntityRenderState;
 import net.minecraft.item.ItemStack;
 
 public class MooseEntityRenderState extends LivingEntityRenderState {
-    // Hardcoded that this is synced server to client
     public ItemStack saddleStack;
 
     public MooseEntityRenderState() {
