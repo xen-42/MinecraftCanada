db84c21e557aec9548eb1aa9c0b8fd1fa07fabf0
xen-42 <nicholasconnors@hotmail.ca>
2025-06-09 23:24:33 -0400

    Wall block works, regular guy dont

--- src/main/java/xen42/canadamod/block/MooseSkullBlock.java
+++ src/main/java/xen42/canadamod/block/MooseSkullBlock.java
@@ -1,13 +1,16 @@
 package xen42.canadamod.block;
 
-import javax.swing.text.html.BlockView;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.SkullBlock;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 
 public class MooseSkullBlock extends SkullBlock {
     public MooseSkullBlock(Settings settings) {
@@ -17,5 +20,15 @@ public class MooseSkullBlock extends SkullBlock {
     @Override
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
         return new MooseSkullBlockEntity(pos, state);
+    }
+
+    @Override
+    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return Block.createColumnShape(12.0, 0.0, 8.0);
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(Properties.POWERED, Properties.ROTATION);
     }
 }
--- src/main/java/xen42/canadamod/block/MooseWallSkullBlock.java
+++ src/main/java/xen42/canadamod/block/MooseWallSkullBlock.java
@@ -1,10 +1,15 @@
 package xen42.canadamod.block;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.SkullBlock.SkullType;
 import net.minecraft.block.WallSkullBlock;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 
 public class MooseWallSkullBlock extends WallSkullBlock {
 
@@ -15,5 +20,10 @@ public class MooseWallSkullBlock extends WallSkullBlock {
     @Override
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
         return new MooseSkullBlockEntity(pos, state);
+    }
+
+    @Override
+    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return (VoxelShape)VoxelShapes.createHorizontalFacingShapeMap(Block.createCuboidZShape(8.0, 0.0, 16.0)).get(state.get(FACING));
     }
 }
